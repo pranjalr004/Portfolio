56bc3d6a96c8468fd1aeffc9d38a0d1680eb1639
pranjalr004 <pranjalr376@gmail.com>
2025-06-16 18:52:55 +0530

    Hero Section Completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { HeroSection } from "../components/HeroSection"
 import { Navbar } from "../components/Navbar"
 import { StarBackground } from "../components/StarBackground"
 import { ThemeToggle } from "../components/ThemeToggle"
@@ -13,7 +14,9 @@ export const Home=()=>{
         <Navbar/>
 
         {/* {Main Content} */}
-        
+        <main>
+            <HeroSection/>
+        </main>
 
         {/* {Footer} */}
         
