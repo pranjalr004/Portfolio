c35b4050b779f99476b4867b89b970e654da3ce2
pranjalr004 <pranjalr376@gmail.com>
2025-06-16 08:55:57 +0530

    Theme Added and Background Effects are done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,20 @@
+import { StarBackground } from "../components/StarBackground"
+import { ThemeToggle } from "../components/ThemeToggle"
+
 export const Home=()=>{
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* {Theme Toggle} */}
         <ThemeToggle/>
 
         {/* {Background Effects} */}
-        <StartBackground/>
+        <StarBackground/>
         {/* {Navbar} */}
-        <Navbar/>
+      
 
         {/* {Main Content} */}
-        <main>
-            <HeroSection/>
-            <AboutSection/>
-            <SkillsScetion/>
-            <ProjectsSection/>
-            <ContactSection/>
-        </main>
+        
 
         {/* {Footer} */}
-        <Footer/>
+        
     </div>
 }
