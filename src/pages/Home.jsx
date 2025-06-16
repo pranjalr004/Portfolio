4fc0a9bb5017b211ce18cb991b52a22859fbc086
pranjalr004 <pranjalr376@gmail.com>
2025-06-16 17:50:25 +0530

    Navbar Section Completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Navbar } from "../components/Navbar"
 import { StarBackground } from "../components/StarBackground"
 import { ThemeToggle } from "../components/ThemeToggle"
 
@@ -9,7 +10,7 @@ export const Home=()=>{
         {/* {Background Effects} */}
         <StarBackground/>
         {/* {Navbar} */}
-      
+        <Navbar/>
 
         {/* {Main Content} */}
         
